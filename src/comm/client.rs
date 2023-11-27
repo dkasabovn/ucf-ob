5cc5be2e6058b0a39303bce5e790766b6c732594
Daniel Kasabov-Nouvion <dkasabovn@gmail.com>
2023-11-27 03:43:53 -0600

    instructions for neo

--- src/comm/client.rs
+++ src/comm/client.rs
@@ -1,40 +1,35 @@
+use crate::comm::urcp::*;
+
 use std::os::unix::net::UnixStream;
 use std::sync::Mutex;
+use std::io::Result;
 
-pub struct Client {
-    inner: Mutex<Inner>,
-}
-
-struct Inner {
+pub struct Inner {
     stream: UnixStream
 }
 
 impl Inner {
-    pub fn new(addr: &'static str) -> Option<Self> {
-        match UnixStream::connect(addr) {
-            Ok(stream) => Some(Inner {
-                stream
-            }),
-            Err(_) => None
-        }
+    pub fn new(addr: &'static str) -> Result<Self> {
+        let stream = UnixStream::connect(addr)?;
+        stream.set_nonblocking(false)?;
+        // TODO: look at these maybe set write_timeout and read_timeout
+        stream.set_read_timeout(None)?;
+        stream.set_write_timeout(None)?;
+        Ok(Self{
+            stream
+        })
+    }
+    pub fn add_order(&mut self, qty: u64, price: i8, ob_id: u16) -> Result<Vec<OBResponseWrapper>> {
+        write_request(&mut self.stream, &OBReqType::ADD, &OBRequest{ add: AddRequest::new(qty, price, ob_id) })?;
+        read_response_vec(&mut self.stream)
+    }
+    pub fn cancel_order(&mut self, oid: usize, ob_id: u16) -> Result<OBResponseWrapper> {
+        write_request(&mut self.stream, &OBReqType::CANCEL, &OBRequest{ cancel: CancelRequest::new(oid, ob_id) })?;
+        read_response(&mut self.stream)
+    }
+    pub fn reduce_order(&mut self, oid: usize, qty: u64, ob_id: u16) -> Result<OBResponseWrapper> {
+        write_request(&mut self.stream, &OBReqType::REDUCE, &OBRequest{ reduce: ReduceRequest::new(oid, qty, ob_id) })?;
+        read_response(&mut self.stream)
     }
 }
 
-impl Client {
-    pub fn new(addr: &'static str) -> Option<Self> {
-        match Inner::new(addr) {
-            Some(client) => Some(Client {
-                inner: Mutex::new(client),
-            }),
-            None => None
-        }
-    }
-
-    pub fn add_order(&mut self, ) {
-        let stream = &self.inner.lock().unwrap().stream;
-    }
-    pub fn cancel_order(&mut self) {
-    }
-    pub fn reduce_order(&mut self) {
-    }
-}
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,3 @@
 pub mod book;
 pub mod comm;
+pub mod store;
