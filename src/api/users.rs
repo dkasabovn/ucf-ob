cc1feb5af6265f2aed6e49e1ea3d3216870694eb
Daniel Kasabov-Nouvion <dkasabovn@gmail.com>
2023-11-27 22:30:51 -0600

    user endpoints done

--- src/api/users.rs
+++ src/api/users.rs
@@ -1,4 +1,5 @@
 use fast_book::comm::client::Client;
+use fast_book::comm::domain::*;
 
 use actix_web::{get,post,Responder,HttpResponse};
 use actix_web::web::Data;
@@ -7,10 +8,16 @@ use firebase_auth::FirebaseUser;
 
 #[post("/user")]
 pub async fn create_user(user: FirebaseUser, client: Data<Client>) -> impl Responder {
-    HttpResponse::Ok().body("howdy")
+    match client.create_user(user.sub) {
+        Some(_) => HttpResponse::Ok().json(GenericResponse{msg: "ok".to_string()}),
+        None => HttpResponse::BadRequest().json(GenericResponse{msg: "err".to_string()})
+    }
 }
 
 #[get("/user")]
 pub async fn get_user(user: FirebaseUser, client: Data<Client>) -> impl Responder {
-    HttpResponse::Ok().body("howdy")
+    match client.get_user(user.sub) {
+        Some(user) => HttpResponse::Ok().json(user),
+        None => HttpResponse::NotFound().json(GenericResponse{msg: "err".to_string()})
+    }
 }
--- src/comm/domain.rs
+++ src/comm/domain.rs
@@ -1,19 +1,29 @@
+use serde::Serialize;
+
+#[derive(Serialize)]
 pub struct User {
     pub id: i32,
     pub sub: String,
     pub balance: i32
 }
 
+#[derive(Serialize)]
 pub struct Contract {
     pub yes_holder: i32,
     pub no_holder: i32,
     pub qty: i32,
 }
 
+#[derive(Serialize)]
 pub struct UserOrder {
     pub id: i32,
     pub book_id: i32,
     pub price: i32,
     pub qty: i32,
     pub user_fk: i32,
+}
+
+#[derive(Serialize)]
+pub struct GenericResponse {
+    pub msg: String
 }
